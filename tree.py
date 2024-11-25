1d75eb0319ad8a8cb6fe8fc9b21c8c8d21518b05
Ignacio Hagopian <jsign.uy@gmail.com>
2024-11-25 20:11:25 -0300

    tree: implement co-location of leaf values
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tree.py
+++ tree.py
@@ -1,7 +1,11 @@
 class LeafNode:
-    def __init__(self, key: bytes, value: bytes):
-        self.key = key
-        self.value = value
+    def __init__(self, stem: bytes):
+        assert len(stem) == 31, "stem must be 31 bytes"
+        self.stem = stem
+        self.values = [b"\x00" * 32] * 256
+
+    def set_value(self, index: int, value: bytes):
+        self.values[index] = value
 
 
 class InternalNode:
@@ -17,60 +21,65 @@ class BinaryTree:
     def insert(self, key: bytes, value: bytes):
         assert len(key) == 32, "key must be 32 bytes"
         assert len(value) == 32, "value must be 32 bytes"
+        stem = key[:31]
+        subindex = key[31]
 
-        key_bits = self._bytes_to_bits(key)
         if self.root is None:
-            self.root = LeafNode(key, value)
+            self.root = LeafNode(stem)
+            self.root.set_value(subindex, value)
             return
 
-        self.root = self._insert_recursive(self.root, key_bits, key, value, 0)
+        self.root = self._insert(self.root, stem, subindex, value, 0)
 
-    def _insert_recursive(self, node, key_bits, key, value, depth):
+    def _insert(self, node, stem, subindex, value, depth):
+        assert depth < 248, "depth must be less than 248"
+
         if node is None:
-            return LeafNode(key, value)
+            node = LeafNode(stem)
+            node.set_value(subindex, value)
+            return node
+
+        stem_bits = self._bytes_to_bits(stem)
         if isinstance(node, LeafNode):
-            if node.key == key:
-                node.value = value
+            if node.stem == stem:
+                node.set_value(subindex, value)
                 return node
-            existing_key_bits = self._bytes_to_bits(node.key)
+            existing_stem_bits = self._bytes_to_bits(node.stem)
             return self._split_leaf(
-                node, key_bits, existing_key_bits, key, value, depth
+                node, stem_bits, existing_stem_bits, subindex, value, depth
             )
 
-        bit = key_bits[depth] if depth < len(key_bits) else 0
+        bit = stem_bits[depth]
         if bit == 0:
-            node.left = self._insert_recursive(
-                node.left, key_bits, key, value, depth + 1
-            )
+            node.left = self._insert(node.left, stem, subindex, value, depth + 1)
         else:
-            node.right = self._insert_recursive(
-                node.right, key_bits, key, value, depth + 1
-            )
+            node.right = self._insert(node.right, stem, subindex, value, depth + 1)
         return node
 
-    def _split_leaf(self, leaf, key_bits, existing_key_bits, key, value, depth):
-        assert depth < len(key_bits), "depth must be less than key length"
-
-        if key_bits[depth] == existing_key_bits[depth]:
+    def _split_leaf(self, leaf, stem_bits, existing_stem_bits, subindex, value, depth):
+        if stem_bits[depth] == existing_stem_bits[depth]:
             new_internal = InternalNode()
-            bit = key_bits[depth]
+            bit = stem_bits[depth]
             if bit == 0:
                 new_internal.left = self._split_leaf(
-                    leaf, key_bits, existing_key_bits, key, value, depth + 1
+                    leaf, stem_bits, existing_stem_bits, subindex, value, depth + 1
                 )
             else:
                 new_internal.right = self._split_leaf(
-                    leaf, key_bits, existing_key_bits, key, value, depth + 1
+                    leaf, stem_bits, existing_stem_bits, subindex, value, depth + 1
                 )
             return new_internal
         else:
             new_internal = InternalNode()
-            bit = key_bits[depth]
+            bit = stem_bits[depth]
+            stem = self._bits_to_bytes(stem_bits)
             if bit == 0:
-                new_internal.left = LeafNode(key, value)
+                new_internal.left = LeafNode(stem)
+                new_internal.left.set_value(subindex, value)
                 new_internal.right = leaf
             else:
-                new_internal.right = LeafNode(key, value)
+                new_internal.right = LeafNode(stem)
+                new_internal.right.set_value(subindex, value)
                 new_internal.left = leaf
             return new_internal
 
@@ -80,3 +89,12 @@ class BinaryTree:
             for i in range(8):
                 bits.append((byte >> (7 - i)) & 1)
         return bits
+
+    def _bits_to_bytes(self, bits):
+        byte_data = bytearray()
+        for i in range(0, len(bits), 8):
+            byte = 0
+            for j in range(8):
+                byte |= bits[i + j] << (7 - j)
+            byte_data.append(byte)
+        return bytes(byte_data)
